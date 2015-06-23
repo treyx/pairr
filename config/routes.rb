7c30428d50d0c4a860ee770125e5d53fac50bcec
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-23 17:36:52 -0600

    added update route for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "dashboard#index"
-  resources :users, only: [:new, :edit]
+  resources :users, only: [:new, :edit, :update]
   get "auth/github/callback", to: "sessions#create"
 end
