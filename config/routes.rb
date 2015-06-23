e089e0cce6aed206a41a1627046066aa17273a72
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-23 16:48:12 -0600

    added edit route/view/controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "dashboard#index"
-  resources :users, only: [:new]
+  resources :users, only: [:new, :edit]
   get "auth/github/callback", to: "sessions#create"
 end
