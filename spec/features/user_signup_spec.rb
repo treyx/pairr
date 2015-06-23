429ef701d15a175c318917f1d938c14007a42745
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-23 13:25:50 -0600

    added extra expectation to drive initial authorization functionality

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.feature "user signup" do
   scenario "should be prompted to login when visiting root" do
     visit root_path
+    expect(current_path).to eq(new_user_path)
     expect(page).to have_content("Please Register an Account")
   end
 end
