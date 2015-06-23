0b890e41d726e334516b279da72cdebe81ba3b5f
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-23 16:45:37 -0600

    added oauth method in user model and updated barebones dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(uid: data.uid)
+
+    user.name = data.info.name
+    user.save
+    user
+  end
 end
