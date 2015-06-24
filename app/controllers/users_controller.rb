1cb5537c34ef56be60ac3e974be2763650149668
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-23 19:13:45 -0600

    fixed and completed stupid form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ class UsersController < ApplicationController
   end
 
   def update
-    binding.pry
+    current_user.update(about: params[:user][:about])
+
+    languages.each do |id|
+      UserLanguage.create(user_id: current_user.id, language_id: id)
+    end
+
+    redirect_to root_path
+  end
+
+  private
+
+  def languages
+    params[:user][:languages].shift
+    params[:user][:languages]
   end
 end
