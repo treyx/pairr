6eaa4295edabf2bfb59e2f7cc044a4bcce2933a0
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-23 19:13:34 -0600

    updated models for join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :languages, through: :user_languages
+  has_many :user_languages
+  validates :about, length: { maximum: 500 }
+
   def self.find_or_create_from_auth(data)
     user = User.find_or_create_by(uid: data.uid)
 
